ecd86c69ad2b44eeb1ce01f960e4cfd072e33cba
Preston Beaty <dennispbeaty@gmail.com>
2022-12-07 11:42:37 -0500

    pt_1 finished
    ...

--- day_7/pt1.py
+++ day_7/pt1.py
@@ -7,35 +7,34 @@ import re
 
 dir = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-input_file = open(os.path.join(dir, 'test.txt'), "r")
+input_file = open(os.path.join(dir, 'input.txt'), "r")
 input = input_file.readlines()
 
 total = 0
 line_count = 0
+folder_sizes = []
+size_tracker = 0
 
 #read each line
 for line in input:
     split_input = line.split()
 
-    if len(split_input == 3):
-
+    if len(split_input) == 3:
         #if you cd into a dir
-        if (split_input[0] == '$' and split_input[0] == 'cd' and split_input[0] != '..'):
-            print("1")
-
+        if (split_input[0] == '$' and split_input[1] == 'cd' and split_input[2] != '..'):
+            folder_sizes.append(0)
         #if you back out of a dir
         else:
-            print("2")
+            if folder_sizes[len(folder_sizes)-1] <= 100000:
+                size_tracker += folder_sizes.pop()
 
     else:
         #If the line is an ls command or a dir listing ine skip
-        if (split_input[0] == 'dir' or split_input[0] == 'ls'):
+        if (split_input[0] == 'dir' or split_input[1] == 'ls'):
             continue
         #Hit a file with an actual size
         elif (split_input[0].isnumeric()):
-            print("you hit an actual file")
+            for i in range(len(folder_sizes)):
+                folder_sizes[i] += int(split_input[0])
 
-    if line_count == 0:
-        print(split_input)
-
-    line_count += 1
+print("Size of folders less than 100,000:", size_tracker)
